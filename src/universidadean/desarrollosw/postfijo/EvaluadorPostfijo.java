1fde182af23f32559c41dd6016711a9008e7d03a
cbustosm0 <cbustosm0@outlook.com>
2021-03-09 23:59:02 -0500

    Evaluador Posfijo

---
+++ src/universidadean/desarrollosw/postfijo/EvaluadorPostfijo.java
@@ -0,0 +1,89 @@
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Universidad Ean (Bogotá - Colombia)
+ * Departamento de Tecnologías de la Información y Comunicaciones
+ * Licenciado bajo el esquema Academic Free License version 2.1
+ * <p>
+ * Proyecto Evaluador de Expresiones Postfijas
+ * Fecha: Febrero 2021
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+package universidadean.desarrollosw.postfijo;
+
+import java.io.IOException;
+import java.io.StreamTokenizer;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Esta clase representa una clase que evalúa expresiones en notación polaca o
+ * postfija. Por ejemplo: 4 5 +
+ */
+public class EvaluadorPostfijo {
+
+    /**
+     * Realiza la evaluación de la expresión postfijo utilizando una pila
+     * @param expresion una lista de elementos con números u operadores
+     * @return el resultado de la evaluación de la expresión.
+     */
+    static int evaluarPostFija(List<String> expresion) {
+        Stack<Integer> pila = new Stack<>();
+
+        for (String s: expresion) {
+            if (s.equals("*") || s.equals("/") || s.equals("+")|| s.equals("-")) {
+                int value1= 0;
+                int value2 = 0;
+                value1 = pila.peek();
+                pila.pop();
+                value2 = pila.peek();
+                pila.pop();
+                switch (s) {
+                    case "*":
+                        pila.push(value2* value1);
+                        break;
+                    case "/":
+                        int r = value2/ value1;
+                        pila.push(r);
+                        break;
+                    case "+":
+                        pila.push(value2+ value1);
+                        break;
+                    case "-":
+                        pila.push(value2- value1);
+                        break;
+
+                }
+
+            } else {
+                pila.push(Integer.parseInt(s));
+            }
+
+        }
+
+        // TODO: Realiza la evaluación de la expresión en formato postfijo
+
+        return pila.peek();
+    }
+
+    /**
+     * Programa principal
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Ingresa una operación posfija, cada valor y operador debe estar separado para ser diferenciado. Ejemplo: 45 7 + \n>");
+        String linea = teclado.nextLine();
+
+        try {
+            List<String> expresion = Token.dividir(linea);
+            System.out.println(evaluarPostFija(expresion));
+        }
+        catch (Exception e) {
+            System.err.printf("Error grave en la expresión: %s", e.getMessage());
+        }
+
+    }
+}
